5e83b5309e98efd440e2c26ccc0d51c46c2957f1
wandexdev <tiwande04@gmail.com>
2022-07-30 16:11:42 +0100

    possible regex constant valid for all scenerios

--- reg.js
+++ reg.js
@@ -3,18 +3,18 @@
 // The pattern should validate emails that have only letters and numbers in the "user" part (the part before the "@")
 // To test your pattern, uncomment the 7 console logs below. In the console, they should all return the expected boolean.
 ​
-const emailRegex = /type-in-here/;
+const emailRegex = /[]wande.dev$/;
 ​
-// console.log(`This should return TRUE: ${emailRegex.test("hello@wande.dev")}`);
+ console.log(`This should return TRUE: ${emailRegex.test("hello@wande.dev")}`);
 ​
-// console.log(`This should return TRUE: ${emailRegex.test("mentoring@wande.dev")}`);
+ console.log(`This should return TRUE: ${emailRegex.test("mentoring@wande.dev")}`);
 ​
-// console.log(`This should return TRUE: ${emailRegex.test("mabel35@wande.dev")}`);
+ console.log(`This should return TRUE: ${emailRegex.test("mabel35@wande.dev")}`);
 ​
-// console.log(`This should return FALSE: ${emailRegex.test("hellowande.dev")}`);
+ console.log(`This should return FALSE: ${emailRegex.test("hellowande.dev")}`);
 ​
-// console.log(`This should return FALSE: ${emailRegex.test("hello@wandedev")}`);
+ console.log(`This should return FALSE: ${emailRegex.test("hello@wandedev")}`);
 ​
-// console.log(`This should return FALSE: ${emailRegex.test("@wande.dev")}`);
+ console.log(`This should return FALSE: ${emailRegex.test("@wande.dev")}`);
 ​
-// console.log(`This should return FALSE: ${emailRegex.test("mabel%45@wande.dev")}`);
+ console.log(`This should return FALSE: ${emailRegex.test("mabel%45@wande.dev")}`);
